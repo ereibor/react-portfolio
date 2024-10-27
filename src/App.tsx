df9ff406eb06c4703d021d4a156d5ca725920c1c
Tunere Ereibor <etunere@gmail.com>
2024-10-27 13:17:36 +0100

    getting started with the header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import Button from "./components/ui/Button";
-import { GithubIcon } from "./helpers/svg";
+import Header from "./components/layout/Header";
+
+
 
 function App() {
   return (
     <>
-      <div className="flex items-center  justify-center gap-4">
-        <h1 className="text-3xl font-bold underline text-red-500 ">
-          Hello world !
-        </h1>
-        <span>{GithubIcon()}</span>
-      </div>
-      <div className="text-center mt-4">
-        <Button variant="secondary">Click me</Button>
-      </div>
+  
+    <Header />
+  
+   
     </>
   );
 }
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,102 @@
+import Link from "next/link";
+import { useState } from "react";
+
+const Header = () => {
+  const [Toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState("#home");
+
+  return (
+    <header className="">
+      <nav className="h-[72px] flex justify-between">
+        <a href="/" className="nav__logo">
+          Tunere
+        </a>
+
+        <div className="">
+          <ul className="flex">
+            <li className="nav__item">
+              <a
+                href="#home"
+                onClick={() => setActiveNav("#home")}
+                className={
+                  activeNav === "#home" ? "nav__link active-link" : "nav__link "
+                }
+              >
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#about"
+                onClick={() => setActiveNav("#about")}
+                className={
+                  activeNav === "#about"
+                    ? "nav__link active-link"
+                    : "nav__link "
+                }
+              >
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#skills"
+                onClick={() => setActiveNav("#skills")}
+                className={
+                  activeNav === "#skills"
+                    ? "nav__link active-link"
+                    : "nav__link "
+                }
+              >
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#services"
+                onClick={() => setActiveNav("#services")}
+                className={
+                  activeNav === "#services"
+                    ? "nav__link active-link"
+                    : "nav__link "
+                }
+              >
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#portfolio"
+                onClick={() => setActiveNav("#portfolio")}
+                className={
+                  activeNav === "#portfolio"
+                    ? "nav__link active-link"
+                    : "nav__link "
+                }
+              >
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#contact"
+                onClick={() => setActiveNav("#contact")}
+                className={
+                  activeNav === "#contact"
+                    ? "nav__link active-link"
+                    : "nav__link "
+                }
+              >
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+          <i onClick={() => showMenu(!Toggle)}></i>
+        </div>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}></div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
