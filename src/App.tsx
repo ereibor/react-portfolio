589ed0c52c8228ade8f67932cdab608a0f1c861b
Tunere Ereibor <etunere@gmail.com>
2024-11-01 00:26:19 +0100

    almost done with hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
 import Header from "./components/layout/Header";
+import Hero from "./components/sections/Hero";
 
 function App() {
   return (
     <>
-      <div className="mx-auto md:max-w-4xl px-4">
-        <Header />
-        <main>
+      <div>
+        <header>
+          <div className="mx-auto md:max-w-4xl px-4">
+            {" "}
+            <Header />
+          </div>
+        </header>
+
+        <main className="mx-auto md:max-w-4xl px-4 mt-20">
           <section className="py-8">
-            {/* Home/Hero section */}
+            <Hero />
           </section>
 
-          <section className="py-8">
-            {/* About section */}
-          </section>
+          <section className="py-8">{/* About section */}</section>
 
-          <section className="py-8">
-            {/* Services section */}
-          </section>
+          <section className="py-8">{/* Services section */}</section>
 
-          <section className="py-8">
-            {/* Contact section */}
-          </section>
+          <section className="py-8">{/* Contact section */}</section>
         </main>
+
+        <footer>
+        <div className="mx-auto md:max-w-4xl px-4">
+            footer
+        
+          </div>
+        </footer>
       </div>
     </>
   );
